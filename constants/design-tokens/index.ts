584f10a80454f938dabd076dacd2012f6fac65e2
melxcreative <melyhaaly@gmail.com>
2026-02-10 06:29:10 -0600

    Add design tokens extracted from Figma

---
+++ constants/design-tokens/index.ts
@@ -0,0 +1,8 @@
+export { colors } from './colors';
+export type { ColorToken } from './colors';
+
+export { typography, fontWeights } from './typography';
+export type { TypographyToken } from './typography';
+
+export { spacing, borderRadius, elevation, layout } from './spacing';
+export type { SpacingToken, BorderRadiusToken, ElevationToken } from './spacing';
